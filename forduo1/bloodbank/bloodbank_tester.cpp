933eab77f17ec58e93c599908a42c6c68a9af765
Matthias Vegh <matyas.vegh@gmail.com>
2014-11-05 23:52:10 +0100

    Refactor bloodbank_tester to have stride as parameter

--- forduo1/bloodbank/bloodbank_tester.cpp
+++ forduo1/bloodbank/bloodbank_tester.cpp
@@ -7,17 +7,20 @@ void runtests(BloodBank& bank)
 {
 	Batch* first= bank.createBatch();
 
-	for(size_t i= 0; i < bank.getNumberOfSamples(); )
-	{
-		size_t testID= first->addTest();
-		first->addSample(testID, i);
-		++i;
-		if (i < bank.getNumberOfSamples())
-		{
-			first->addSample(testID, i);
-			++i;
+	const size_t stride = 2;
+	for(size_t i = 0; i < bank.getNumberOfSamples(); i+=stride) {
+		size_t testId = first->addTest();
+		for(size_t j = 0; j < stride; ++j) {
+			first->addSample(testId, j+i);
 		}
 	}
+	if (bank.getNumberOfSamples() % stride) {
+		size_t remainderId = first->addTest();
+		for(size_t i = 0; i < bank.getNumberOfSamples()%stride; ++i) {
+			first->addSample(remainderId, i);
+		}
+	}
+
 	bank.evaluateBatch(first);
 
 	Batch* second= bank.createBatch();
