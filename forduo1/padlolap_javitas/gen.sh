0927f83c482f9815c7eac6606de665086c0e002f
Peter Szabados <kangirigungi@gmail.com>
2014-11-09 10:34:23 +0100

    Generate more outputs randomly

---
+++ forduo1/padlolap_javitas/gen.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ -n "$1" ]; then
+	seed=$1
+else
+	seed=0
+fi
+
+for ((i=0; i<1000; ++i)); do
+	name=tiles2_gen_$i
+	echo $name >&2
+	targetName=${name}.target
+	outputName=${name}.out
+	if ./build-clang_release/bin/findTarget tiles2.txt $((seed + i)) >$targetName; then
+		./build-clang_release/bin/padlolap tiles2.txt $targetName >$outputName
+	fi
+done
+
+echo
+echo
+for f in *.out; do
+	echo $(head -n1 $f) $f
+done | sort -n
+
--- forduo1/padlolap_javitas/src/TargetStatus.cpp
+++ forduo1/padlolap_javitas/src/TargetStatus.cpp
@@ -4,6 +4,7 @@
 #include "DumperFunctions.hpp"
 #include <boost/lexical_cast.hpp>
 #include <iostream>
+#include <stdexcept>
 #include <boost/random/mersenne_twister.hpp>
 #include <boost/random/uniform_int_distribution.hpp>
 
@@ -19,34 +20,45 @@ struct ColorData {
 Status findTargetStatus(const Status& status, unsigned seed)
 {
 	ColorData data[3];
-	Status result = status;
-	result.field.fill(-1);
 	for (Point p: arrayRange(status.field)) {
 		++data[status.field[p]].numberOfTiles;
 	}
 
-	boost::random::mt19937 rng{seed};
-	boost::random::uniform_int_distribution<std::size_t> randomColumn{0, status.field.width() - 1};
-	boost::random::uniform_int_distribution<std::size_t> randomRow{0, status.field.height() - 1};
+		boost::random::mt19937 rng{seed};
+		boost::random::uniform_int_distribution<std::size_t> randomColumn{0, status.field.width() - 1};
+		boost::random::uniform_int_distribution<std::size_t> randomRow{0, status.field.height() - 1};
+	for (int i = 0; i < 100; ++i) {
 
-	std::vector<std::pair<Point, std::size_t>> startingPoints;
-	for (int i = 0; i < 3; ++i) {
-		data[i].referencePoint = Point{static_cast<int>(randomColumn(rng)),
-			static_cast<int>(randomRow(rng))};
-		result.field[data[i].referencePoint] = i;
-		startingPoints.emplace_back(data[i].referencePoint, data[i].numberOfTiles);
-	}
-	int tries = 1000;
-	auto field = result.field;
-	while (tries > 0 && !grow(field, startingPoints, [&result](Point p, int)
-			{
-				return result.field[p] < 0;
-			}, rng)) {
-		--tries;
-		field = result.field;
+		std::vector<std::pair<Point, std::size_t>> startingPoints;
+		Status result = status;
+		result.field.fill(-1);
+		for (int i = 0; i < 3; ++i) {
+			do {
+				data[i].referencePoint = Point{static_cast<int>(randomColumn(rng)),
+					static_cast<int>(randomRow(rng))};
+			} while (result.field[data[i].referencePoint] != -1);
+
+			result.field[data[i].referencePoint] = i;
+			startingPoints.emplace_back(data[i].referencePoint, data[i].numberOfTiles);
+		}
+		int tries = 1000;
+		auto field = result.field;
+		bool ok = false;
+
+		while (tries > 0 && !(ok = grow(field, startingPoints, [&result](Point p, int)
+				{
+					return result.field[p] < 0;
+				}, rng))) {
+			--tries;
+			field = result.field;
+		}
+
+		if (ok) {
+			result.field = field;
+			return result;
+		}
 	}
 
-	result.field = field;
-	return result;
+	throw std::logic_error{"did not succeed"};
 }
 
