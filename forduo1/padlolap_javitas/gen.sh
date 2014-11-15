1a0158f46525a405926d5b8f8f21c0dce88fd7b6
Peter Szabados <kangirigungi@gmail.com>
2014-11-15 10:01:27 +0100

    gen.sh only keeps the best solution

--- forduo1/padlolap_javitas/gen.sh
+++ forduo1/padlolap_javitas/gen.sh
@@ -8,6 +8,9 @@ else
 	seed=0
 fi
 
+best=
+bestFiles=
+
 for ((i=0; i<1000; ++i)); do
 	currentSeed=$((seed + i))
 	name=${basename}_gen_$currentSeed
@@ -16,6 +19,15 @@ for ((i=0; i<1000; ++i)); do
 	outputName=${name}.out
 	if ./build-clang_release/bin/findTarget ${basename}.txt $currentSeed >$targetName; then
 		./build-clang_release/bin/padlolap ${basename}.txt $targetName >$outputName
+
+		value=$(head -n1 $outputName)
+		if [ -z "$best" ] || [ "$best" -gt "$value" ]; then
+			best=$value
+			rm -f $bestFiles
+			bestFiles="$outputName $targetName"
+		else
+			rm $outputName $targetName
+		fi
 	fi
 done
 
